cac9455b5e9f0649552803314fbf33bf326914b8
muneeb786-pixel <muneeburr451@gmail.com>
2024-03-07 13:08:14 +0500

    UTILS, ERROR AND RESPONSE Classes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on('error',(error)=>{
+        console.log(`EXPRESS ERROR ${error}`);
+        process.exit(1)
+    })
 
-// ;(async ()=>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     } catch (error) {
-//         console.log("DB ERROR ",error)
-//         throw error;
-//     }
-// })()
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB CONNECTION FAILED", err);
+  });
